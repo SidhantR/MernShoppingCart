6ceea882014a76486162b9c3761b04878bd3b5e3
Sidhant <sidhantrc02@gmail.com>
2021-03-28 18:52:15 +0530

    ready

--- backend/data/products.js
+++ backend/data/products.js
@@ -11,7 +11,7 @@ const products = [
       countInStock: 15,
     },
     {
-      name: "Iphone 12",
+      name: "IPhone 12",
       imageUrl:
         "https://images.unsplash.com/photo-1605787020600-b9ebd5df1d07?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1463&q=80",
       description:
